449853c23f19bc0319849c6b4b709618d4a2d0bd
sebenarnyainginbertanya <165452238+sebenarnyainginbertanya@users.noreply.github.com>
2024-03-30 11:41:36 +0700

    init

---
+++ test.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.header('Aplikasi Penjumlahan Bilangan Numerik', divider='rainbow')
+
+angka_pertama = st.number_input('Masukkan Angka Pertama')
+st.write('The First Number Is ', angka_pertama)
+
+angka_kedua = st.number_input('Masukkan Angka Kedua')
+st.write('The Second Number Is ', angka_kedua)
+
+operasi_matematika = angka_pertama / angka_kedua
+st.write(f"Angka Pertama {angka_pertama} x angka kedua (angka_kedua) = (operasi_matematika)")
